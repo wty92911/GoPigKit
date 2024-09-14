86bbde46d15832ba86894b8d35b3c38632da4205
wty92911 <2066946886@qq.com>
2024-09-14 14:41:06 +0800

    :card_file_box: change CreateBy's gorm tag

--- internal/model/menu.go
+++ internal/model/menu.go
@@ -5,6 +5,6 @@ type MenuItem struct {
 	FamilyID  *uint   `gorm:"primaryKey;not null" json:"family_id"`                  // 所属家庭ID
 	FoodID    *uint   `gorm:"primaryKey;not null" json:"food_id" binding:"required"` // 食品ID
 	Quantity  uint    `json:"quantity" binding:"required"`                           // 数量
-	CreatedBy *string `json:"created_by"`                                            // 创建者
+	CreatedBy *string `gorm:"type:varchar(255);not null" json:"created_by"`          // 创建者
 	User      *User   `gorm:"foreignKey:CreatedBy;references:OpenID"`
 }
--- internal/model/order.go
+++ internal/model/order.go
@@ -7,16 +7,15 @@ import (
 // Order 表示一个订单
 type Order struct {
 	gorm.Model
-	FamilyID *uint        `gorm:"index;not null" json:"family_id"`                              // 家庭ID，外键
-	Items    []*OrderItem `gorm:"foreignKey:OrderID;constraint:OnDelete:CASCADE;" json:"items"` // 关联的订单项，联级删除
-
+	FamilyID *uint        `gorm:"index;not null" json:"family_id"`
+	Items    []*OrderItem `gorm:"foreignKey:OrderID;constraint:OnDelete:CASCADE;" json:"items" binding:"required"`
 }
 
 // OrderItem 表示订单中的一个项
 type OrderItem struct {
-	OrderID   *uint   `gorm:"primaryKey;not null" json:"order_id"`          // 订单ID，外键
-	FoodID    *uint   `gorm:"primaryKey;not null" json:"food_id"`           // 食品ID
-	Quantity  int     `json:"quantity"`                                     // 数量
-	CreatedBy *string `gorm:"type:varchar(255);not null" json:"created_by"` // 创建者，外键
+	OrderID   *uint   `gorm:"primaryKey;not null" json:"order_id"`
+	FoodID    *uint   `gorm:"primaryKey;not null" json:"food_id" binding:"required"`
+	Quantity  uint    `json:"quantity" binding:"required"`
+	CreatedBy *string `gorm:"type:varchar(255);not null" json:"created_by"`
 	User      *User   `gorm:"foreignKey:CreatedBy;references:OpenID"`
 }
